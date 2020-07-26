e8e1537912e10668577d3e7591a9083eb9f12c85
gama66 <g.marrella@erinformatica.it>
2020-07-26 11:34:40 +0200

    Update Hasher

--- hasher.php
+++ hasher.php
@@ -1,85 +1,171 @@
 <?php
 require 'config.php';
+/*
+	Generates
+*/
+$hasher = new Hasher('acskodsamk mklasdk lmdakls');
 
-function sentencer( $sentence, $words, $count, $ALGO = "crc32"){
-	$strArr = str_split($sentence);//explode(" ", $sentence);
-
-	$sentence = [];
-	foreach( $strArr as $word){
-		$hashed = hash($ALGO, worder($word));
-		$hashedHex = ToHex($hashed);
-		$mod = bcmod($hashedHex, $count);
-		$sentence[] = trim($words[$mod]);
-	}
-	return implode(" ", $sentence);
+foreach($hasher->getSentences() as $sentence){
+	echo "$sentence\n";
 }
 
-function worder($word){
-	$wordSize = strlen($word);
-	$pos = $wordSize % 22;
-	$str_hex = strtoupper(ToHex($word));
-	$secondtable = array(43, 31, 11, 79, 43, 36, 53, 54, 91, 55,
-						 83, 67, 21, 22, 11, 37, 75, 12, 12, 88,
-						 43, 50, 99);
-	$hashed = strtoupper(ToHex(chr($secondtable[$pos])));
-	for($i = 0; $i < strlen($str_hex); $i += 2)
-	{
-		$hashed .= strtoupper(ToHex(chr(($secondtable[$pos] & $wordSize) >> 4)));
-		$hashed .= $str_hex[$i];
-		$hashed .= strtoupper(ToHex(chr($secondtable[$pos] & $wordSize)));
-		$hashed .= $str_hex[$i + 1];
-		$pos == $wordSize ? $pos = 0 : $pos++;
-	}
-	return $hashed;
-}
 
-function ToHex($string)
-{
-	$hex = "";
-	for ($i = 0; $i < strlen($string); $i++)
-		$hex .= dechex(ord($string[$i]));
-	return $hex;
-}
+echo "\n\n\n\n" . $hasher->getParagraph();
 
-function link_generator( $sentence ) {
+/**
+ * [Description Hasher]
+ */
+class Hasher {
 
-	// TODO: put this in config file
-	$DICTIONARY_FILE = ABSPATH . '/words.txt';
-	$words = file( $DICTIONARY_FILE );
-	$count = count( $words );
-	$ALGO = "sha512";
+	/**
+	 * @var [string]
+	 */
+	public $word;
+	public $seed;
 
-	$sentence = sentencer($sentence, $words, $count, $ALGO);
-	$sentenceLenght = strlen($sentence);
-	$wordCount = str_word_count($sentence);
-	
-	$linksPerSentence = $sentenceLenght % $wordCount;
-	$exploded = explode(" ", $sentence);
-	
-	for($i = 0; $i < $wordCount; $i+= $linksPerSentence){
-		$url = sprintf(
-			URL_FORMAT,
-			urlencode( $exploded[$i] )
-		);
-		$exploded[$i] = "<a href=\"$url\">{$exploded[$i]}</a>";
+	function __construct($word){
+		$this->word = $word;
+		$this->init();
 	}
 
-	return implode(" ", $exploded);
+	/**
+	 * This initialize
+	 */
+	function init(){
+		$seed = $this->updateSeed($this->word);
+		mt_srand($seed);
+	}
+
+	function updateSeed($word){
+		$this->seed = $seedVal = $this->getBinaryWord($word);
+		return intval($this->seed);
+	}
+
+	/**
+	 * @return string Body Generated from a word
+	 */
+	function getBody(){
+		$body = '';
+		$seedSplit = str_split($this->seed, intval($this->seed) % 6 + 1);
+		foreach($seedSplit as $times){
+			$body .= str_shuffle('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_');
+		}
+        return $body;
+	}
+
+	/**
+	 * @return string Body Generated from a word
+	 */
+	function getUrl(){
+		return str_shuffle('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_');
+	}
+
+	/**
+	 * @param  $seed if not passed uses as seed $this->getUrl();
+	 * @return [string] return a sentence
+	 */
+	function getSentence($seed =null){
+		$seed = $seed ?? $this->getBody();
+		return Sentencer::getSentence($seed);
+	}
+
+	/**
+	 * @param mixed $seed
+	 * 
+	 * @return array[string] list of sentences
+	 */
+	function getSentences(){
+		$sentences = [];
+		$seed = $seed ?? $this->getBody();
+		$chunks = str_split($seed, (intval($seed) % 6 +1));
+		foreach($chunks as $chunk) {
+			$sentences[] = $this->getSentence($chunk);
+		}
+		return $sentences;
+	}
+
+	/**
+	 * @param mixed $seed
+	 * 
+	 * @return array[string] list of sentences
+	 */
+	function getParagraph(){
+		return Sentencer::getParagraph();
+	}
+
+    function getBinaryWord($word){
+		$binWord = "";
+		for($i = 0; $i < strlen($word); $i++) {
+			$binWord .= ord($word[$i]);
+		}
+		return $binWord;
+	}
+
 }
 
-function bcmod( $x, $y ) 
-{ 
-    // how many numbers to take at once? carefull not to exceed (int) 
-    $take = 5;     
-    $mod = ''; 
 
-    do 
-    { 
-        $a = (int)$mod.substr( $x, 0, $take ); 
-        $x = substr( $x, $take ); 
-        $mod = $a % $y;    
-    } 
-    while ( strlen($x) ); 
+class Sentencer {
 
-    return (int)$mod; 
+	static function getParagraph(){
+		$paragraph = "";
+		for($i = 0; $i < (rand() % 1000 + 2); $i++){
+			$val = Sentencer::getSentence();
+			$paragraph .= $val;
+			if(rand() % 1000 <= 100){
+				$paragraph.="\n\n\n";
+			}
+			if(rand() % 1000 <= 250){
+				$paragraph.="\n";
+			}
+		}
+		return $paragraph;
+	}
+
+	static function getSentence(){
+		return sprintf("%s %s %s %s.", 
+		Sentencer::getSubject(),
+		Sentencer::getVerb(),
+		Sentencer::getObject(),
+		// Sentencer::getManner($word),
+		// Sentencer::getPlace($word),
+		 Sentencer::getTime()
+	    );
+	}
+
+	static function getSubject(){
+		$input = array("Neo", "Morpheus", "Trinity", "Cypher", "Tank");
+		shuffle($input);
+		return $input[0];
+	}
+
+	static function getVerb(){
+		$input = array("eat", "play", "fuck");
+		shuffle($input);
+		return $input[0];
+	}
+
+	static function getObject(){
+		$input = array("Neo", "Morpheus", "Trinity", "Cypher", "Tank");
+		shuffle($input);
+		return $input[0];
+	}
+
+	// static function getManner($word){
+	// 	$input = array("Neo", "Morpheus", "Trinity", "Cypher", "Tank");
+	// 	shuffle($input);
+	// 	return $input[0];
+	// }
+	// static function getPlace($word){
+	// 	$input = array("Neo", "Morpheus", "Trinity", "Cypher", "Tank");
+	// 	shuffle($input);
+	// 	return $input[0];
+	// }
+	static function getTime(){
+		$input = array("today", "tomorrow", "now", "on Monday", "on Tuesday");
+		shuffle($input);
+		return $input[0];
+	}
+
+
+
 }
